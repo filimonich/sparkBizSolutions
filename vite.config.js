01189f20beed98da0e6a9e1c190c74bb98fa1333
Sergey Filimonov <srfilimonoff@gmail.com>
2023-12-07 18:29:49 +0300

    refactor: update build

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import autoprefixer from 'autoprefixer';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     {
-      apply: 'build',
+      apply: "build",
       postcss: {
         plugins: [autoprefixer()],
       },
     },
   ],
   build: {
-    outDir: resolve(__dirname, 'docs'), // Обновляем путь для сборки
+    outDir: resolve(__dirname, "docs"),
+    base: "./", // Установите base: './'
   },
 });
