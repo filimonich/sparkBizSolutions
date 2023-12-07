34c56a474343335bdb042f3dc6369892df4bfbac
Sergey Filimonov <srfilimonoff@gmail.com>
2023-12-07 18:15:18 +0300

    feat: add reset button

--- src/components/ClickableButton.vue
+++ src/components/ClickableButton.vue
@@ -1,24 +1,28 @@
 <template>
   <div>
-    <button @click="incrementCount" :style="{ backgroundColor: buttonColor }">
-      Click Me
-    </button>
+    <button v-on:click="incrementCount">Нажми меня!</button>
     <span>{{ clickCount }}</span>
+    <button class="reset" @click="resetCount">Сбросить счетчик</button>
   </div>
 </template>
 
 <script>
+// Экспортируемый объект, представляющий компонент Vue
 export default {
+  // Метод data, возвращающий объект с данными компонента
   data() {
+    // Возвращаем объект с данными компонента
     return {
-      buttonColor: "red",
-      clickCount: 0,
+      clickCount: 0, // Количество кликов
     };
-  },
+  }, // Методы компонента
   methods: {
+    // Метод для увеличения счетчика кликов
     incrementCount() {
-      this.clickCount++;
-      this.buttonColor = this.buttonColor === "red" ? "blue" : "red";
+      this.clickCount++; // Увеличиваем счетчик кликов на 1 при каждом нажатии на кнопку
+    },
+    resetCount() {
+      this.clickCount = 0; // Сбрасываем счетчик кликов на 0
     },
   },
 };
@@ -30,12 +34,22 @@ button {
   font-size: 16px;
   color: white;
   border: none;
+  background-color: red;
   cursor: pointer;
+}
+
+button:active {
+  border: none;
+  background-color: blue;
 }
 
 span {
   display: block;
   width: 20px;
   margin: 10px auto;
+}
+
+.reset {
+  background-color: green;
 }
 </style>
